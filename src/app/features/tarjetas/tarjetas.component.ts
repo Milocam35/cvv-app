10d858295ea5d80c10aa100f2cc59a52eb5425bb
camilo <camimillan2003@gmail.com>
2025-05-21 00:00:23 -0500

    agregando pagina de home con estilos

---
+++ src/app/features/tarjetas/tarjetas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjetas',
+  imports: [],
+  templateUrl: './tarjetas.component.html',
+})
+export class TarjetasComponent {
+
+}
